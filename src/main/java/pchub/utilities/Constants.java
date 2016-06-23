6793d34001c65e979a4f7302f28cf2cf1a061b8f
daisyab <nkweteyimdaisy@gmail.com>
2016-06-23 11:27:22 +0100

    Constants file for constants used in tests

---
+++ src/main/java/pchub/utilities/Constants.java
@@ -0,0 +1,27 @@
+package pchub.utilities;
+
+public class Constants {
+	
+	public static final String baseURL = "http://localhost:3000/";
+	
+	public static final String index = "file:///home/daisy/pchub/prototype/index.html";
+	
+	public static final String login = "file:///home/daisy/pchub/prototype/login.html";
+	
+	public static final String adminXpath = "/html/body/div/div/div[2]/div/div/div/div/div[2]/form/div[3]/label[1]";
+	
+	public static final String staffXpath = "/html/body/div/div/div[2]/div/div/div/div/div[2]/form/div[3]/label[2]";
+			
+	public static final String volXpath = "/html/body/div/div/div[2]/div/div/div/div/div[2]/form/div[3]/label[3]";
+	
+	public static final String signupSubmit = "/html/body/div/div/div[2]/div/div/div/div/div[2]/form/button";
+	
+	public static final String loginDataSheetName = "Sheet 1";
+	
+	public static final String pathToExcel = "src/main/java/pchub/utilities/ExcelSheet.xlsx";
+	
+	public static final String webapp = baseURL + "web_list.html";
+	
+	public static final String mobileapp = baseURL + "app_list.html";
+
+}
