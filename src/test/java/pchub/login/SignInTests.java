e637354d1d2914cee199a7f4a3d7a5c3782693c3
daisyab <nkweteyimdaisy@gmail.com>
2016-06-27 06:08:56 +0100

    Removed test for types of users as they have not been implemented in PCHub yet

--- src/test/java/pchub/login/SignInTests.java
+++ src/test/java/pchub/login/SignInTests.java
@@ -4,38 +4,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import pchub.utilities.CommonCode1;
+import pchub.utilities.CommonCode;
 import pchub.utilities.Constants;
 import pchub.utilities.ExcelReader;
 
 
-public class SignInTests extends CommonCode1 {
+public class SignInTests extends CommonCode {
 	
 	@Test(dataProvider = "Authentication")
-	public void signIn(String email, String password, char user) {
+	public void signIn(String email, String password) {
+		driver.findElement(By.cssSelector("a[href='/users/sign_in']")).click();
 		
-		WebElement mail = driver.findElement(By.id("exampleInputEmail1"));
+		
+		WebElement mail = driver.findElement(By.id("user_email"));
 		mail.sendKeys(email);
 		
-		WebElement passwd = driver.findElement(By.id(""));
+		WebElement passwd = driver.findElement(By.id("user_password"));
 		passwd.sendKeys(password);
-		
+	/*	
 		switch (user) {
-		case 'A': 	WebElement uservar = driver.findElement(By.xpath(Constants.adminXpath));
+		case "Administrator": 	WebElement uservar = driver.findElement(By.xpath(Constants.adminXpath));
 								uservar.click();
 								break;
 		
-		case 'S': 		WebElement uservariable = driver.findElement(By.xpath(Constants.staffXpath));
+		case "Staff": 	WebElement uservariable = driver.findElement(By.xpath(Constants.staffXpath));
 								uservariable.click();
 								break;
 							
-		case 'V': 	WebElement uservariab = driver.findElement(By.xpath(Constants.volXpath));
+		case "Volunteer": 	WebElement uservariab = driver.findElement(By.xpath(Constants.volXpath));
 								uservariab.click();
 								break;					
 		default:				System.out.println("Not given");
-		}
+		}*/
 		
-		WebElement submit = driver.findElement(By.xpath(Constants.signupSubmit));
+		WebElement submit = driver.findElement(By.name("commit"));
 		submit.click();
 	}
 	
