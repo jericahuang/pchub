f8c75629d7608f98e431bd2d680498b0a676ffd2
daisyab <nkweteyimdaisy@gmail.com>
2016-07-13 05:03:52 +0100

    Include @AfterTest and @BeforeTest in script to find the signup button

--- src/test/java/pchub/login/SignInTests.java
+++ src/test/java/pchub/login/SignInTests.java
@@ -1,46 +1,56 @@
 package pchub.login;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import pchub.utilities.CommonCode;
 import pchub.utilities.Constants;
 import pchub.utilities.ExcelReader;
+import org.testng.Assert;
 
-
-public class SignInTests extends CommonCode {
+public class SignInTests {
+	private WebDriver driver;
+	private String expectedTitle = "PcHub";
+	
+	@BeforeTest
+	public void beforeTest() {	
+		String baseURL = Constants.baseURL;
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseURL);
+		driver.manage().window().maximize();
+		WebElement signInButton = driver.findElement(By.cssSelector("a[href='/users/sign_in']"));
+		signInButton.click();
+	}
 	
 	@Test(dataProvider = "Authentication")
 	public void signIn(String email, String password) {
-		driver.findElement(By.cssSelector("a[href='/users/sign_in']")).click();
-		
 		
 		WebElement mail = driver.findElement(By.id("user_email"));
+		mail.clear();
 		mail.sendKeys(email);
 		
 		WebElement passwd = driver.findElement(By.id("user_password"));
+		passwd.clear();
 		passwd.sendKeys(password);
-	/*	
-		switch (user) {
-		case "Administrator": 	WebElement uservar = driver.findElement(By.xpath(Constants.adminXpath));
-								uservar.click();
-								break;
-		
-		case "Staff": 	WebElement uservariable = driver.findElement(By.xpath(Constants.staffXpath));
-								uservariable.click();
-								break;
-							
-		case "Volunteer": 	WebElement uservariab = driver.findElement(By.xpath(Constants.volXpath));
-								uservariab.click();
-								break;					
-		default:				System.out.println("Not given");
-		}*/
+	
+		//WebElement checkbox = driver.findElement(By.id("user_remember_me"));
+		//checkbox.click();
 		
 		WebElement submit = driver.findElement(By.name("commit"));
 		submit.click();
+		
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+		Assert.assertTrue(driver.getPageSource().contains("Signed in successfully."));
 	}
-	
+		
 	@DataProvider(name="Authentication")
 	public Object[][] signInDataProvider() throws Exception {
 		
@@ -48,5 +58,10 @@ public class SignInTests extends CommonCode {
 	    		Constants.loginDataSheetName);
 	    Object[][] cellData = ExcelReader.read(connect);
 		return cellData;
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
 	}
 }
