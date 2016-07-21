4dad303fce86a3c3800c827afadebe7d512f7a2d
Daisy Nkweteyim <daisyab@users.noreply.github.com>
2016-07-21 16:29:29 +0100

    Reduced the indent size to '2' and added comments

--- pc-hub/spec/controllers/users_controller_spec.rb
+++ pc-hub/spec/controllers/users_controller_spec.rb
@@ -1,20 +1,26 @@
+=begin
+This file tests the creation of a user with valid and invalid credentials.
+=end
+
 require 'spec_helper'
 
 describe 'UsersController' do
 	describe 'POST #create' do
+		
+		#create new user - valid credentials.
   	context 'with valid credentials' do
     	it 'creates user and logs in' do
-      #	user = create(:user)
       	post :create, user: attributes_for(:user)
 				expect(controller.current_user).to eq user
 			end
 
 			it 'redirects to the "show" action for the new user' do
 				post :create, user: attributes_for(:user)
-		    expect(response).to render_template(:show) #was :new before
+		    expect(response).to render_template(:show)
 			end
     end
-
+    
+		#create new user - invalid credentials. 
 		context 'with invalid credentials' do
 			it 'does not create a new user' do
 				post :create, user: attributes_for(:user, password: 'invalid')
@@ -26,15 +32,5 @@ describe 'UsersController' do
 				expect(response).to render_template :new
 			end
 		end
-
-  #context 'when password is valid' do
-    #it 'sets the user in the session and redirects them to their dashboard' do
-      #user = create(:user)
-
-     # post :create, session: { email: user.email, password: user.password }
-
-     # expect(response).to redirect_to '/dashboard'
-     # expect(controller.current_user).to eq user
-    #end
   end
 end
