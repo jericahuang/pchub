33d7e008bc3173e581e980ef30ccb930537c08db
Jerica Huang <jerica.huang@gmail.com>
2018-06-05 21:06:38 -0400

    add missing field

--- pc-hub/db/schema.rb
+++ pc-hub/db/schema.rb
@@ -300,6 +300,7 @@ ActiveRecord::Schema.define(version: 20180531213250) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved"
