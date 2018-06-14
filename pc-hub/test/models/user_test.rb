d64da21414372c39000107bf718432dbed281483
Jerica Huang <jerica.huang@gmail.com>
2018-06-14 04:16:19 -0400

    e-mail tests

--- pc-hub/test/models/user_test.rb
+++ pc-hub/test/models/user_test.rb
@@ -1,7 +1,30 @@
-require 'test_helper'
-
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test  "shouldn't allow non-peacecorps.gov email" do
+    user = User.new(email: 'asdfjkl@gmail.com')
+    assert_not user.valid?
+    assert_equal ["Must be a peacecorps.gov account"], user.errors.messages[:email]
+  end
+
+  test  "shouldn't allow peacecorps.gov email without username" do
+    user = User.new(email: '@peacecorps.gov')
+    assert_not user.valid?
+    assert_equal ["Invalid Email", "Must be a peacecorps.gov account"], user.errors.messages[:email]
+  end
+
+  test  "shouldn't allow trailing chars after peacecorps.gov" do
+    user = User.new(email: 'asdf@peacecorps.govn')
+    assert_not user.valid?
+    assert_equal ["Must be a peacecorps.gov account"], user.errors.messages[:email]
+  end
+
+  test  "shouldn't allow invalid e-mail" do
+    user = User.new(email: 'asdf')
+    assert_not user.valid?
+    assert_equal ["Invalid Email", "Must be a peacecorps.gov account"], user.errors.messages[:email]
+  end
+
+  test  "should allow peacecorps.gov email" do
+    user = User.new(email: 'hello@peacecorps.gov')
+    assert_empty user.errors[:email]
+  end
 end
