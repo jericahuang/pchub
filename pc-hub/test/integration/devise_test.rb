540b25fe3f4f46f79bb427fa2a76ece55ccd99ea
Jerica Huang <jerica.huang@gmail.com>
2018-06-25 05:16:00 -0400

    progress

--- pc-hub/test/integration/devise_test.rb
+++ pc-hub/test/integration/devise_test.rb
@@ -7,21 +7,12 @@ class DeviseTest < ActionDispatch::IntegrationTest
     Warden.test_mode!
   end
 
-  test "logged in message" do
-    get root_path
-    assert_redirected_to new_user_session_path
-    follow_redirect!
-    assert_select 'h2', 'Log in'
-  end
-
   test "register new user" do
-    user = User.create! name: 'Joe', email: 'foo@example.com', password: '12345678', country: 'USA', city: 'NY', email: 'joe@peacecorps.gov', role: 'volunteer'
-    exception = assert_raises(Exception) {login_as(user, :scope => :user)}
-    #user.confirmed_at = Time.zone.now
+    user = User.create! name: 'Joe', email: 'foo@example.com', password: 'mypassword', password_confirmation: 'mypassword', country: 'USA', city: 'NY', email: 'joe@peacecorps.gov', email_confirmation: 'joe@peacecorps.gov', role: 'volunteer'
+    user.confirmed_at = Time.zone.now
     login_as(user, :scope => :user)
 
     get root_path
-    assert_equal root_path, current_path
-    assert_select 'h1', 'Welcome, foo@example.com!'
+    assert_redirected_to root_path
   end
 end
